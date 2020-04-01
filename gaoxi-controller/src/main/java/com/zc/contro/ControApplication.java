6e4732ec21f16e33789202318df27e7d33844ee7
zhouchao1992 <1669028439@qq.com>
2020-04-01 17:29:51 +0800

    1

---
+++ gaoxi-controller/src/main/java/com/zc/contro/ControApplication.java
@@ -0,0 +1,13 @@
+package com.zc.contro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ControApplication.class, args);
+    }
+
+}
---
+++ gaoxi-controller/src/test/java/com/zc/contro/ControApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zc.contro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
